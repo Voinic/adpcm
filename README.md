25daafa7fc75c61d843dddf78dc6610790321db4
Voinic <oggy@is.ua>
2024-07-23 19:34:03 +0300

    optimized for MicroPython

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# Streaming Audio Compression
+# MicroPython IMA ADPCM Streaming Audio Compression
 
-Proof of concept test exploring the aptX / ADPCM codecs used in streaming Bluetooth audio. Codec chosen for minimal reliance on floating point math - potentially suitable for embedded implementation, which would be the goal.
+Proof of concept test exploring the aptX / ADPCM codecs used in streaming Bluetooth audio. Codec chosen for minimal reliance on floating point math - potentially suitable for embedded implementation, which would be the goal. Optimized for execution on MicroPython platform.
+
+## Installation using MIP
+
+```python
+import mip
+mip.install("github:Voinic/adpcm")
+```
+
+## Usage
+
+```python
+import array
+from adpcm import encode, decode
+
+audio = array.array('h', [-100, -50, 0, 50, 100])
+print("Input audio:", audio)
+
+# encode
+audio_adpcm = encode(audio)
+print("Encoded audio:", audio_adpcm)
+
+# save or transmit audio_adpcm
+
+# decode
+_audio = decode(audio_adpcm)
+print("Decoded audio:", _audio)
+```
+
+### Output
+
+```text
+Input audio: array('h', [-100, -50, 0, 50, 100])
+Encoded audio: array('b', [15, 15, 4, 5, 3])
+Decoded audio: array('h', [-11, -41, -3, 53, 105])
+```
--- adpcm.py
+++ adpcm.py
@@ -1,9 +1,13 @@
-ima_index_table = [
+import micropython
+import array
+
+
+IMA_INDEX_TABLE = micropython.const((
   -1, -1, -1, -1, 2, 4, 6, 8,
   -1, -1, -1, -1, 2, 4, 6, 8
-]
+))
 
-ima_step_table = [
+IMA_STEP_TABLE = micropython.const((
   7, 8, 9, 10, 11, 12, 13, 14, 16, 17,
   19, 21, 23, 25, 28, 31, 34, 37, 41, 45,
   50, 55, 60, 66, 73, 80, 88, 97, 107, 118,
@@ -13,26 +17,35 @@ ima_step_table = [
   2272, 2499, 2749, 3024, 3327, 3660, 4026, 4428, 4871, 5358,
   5894, 6484, 7132, 7845, 8630, 9493, 10442, 11487, 12635, 13899,
   15289, 16818, 18500, 20350, 22385, 24623, 27086, 29794, 32767
-]
+))
+
+ima_index_table = array.array("i", IMA_INDEX_TABLE)
+ima_step_table = array.array("i", IMA_STEP_TABLE)
 
 
-def encoder(samples):
-    encoded_samples = []
-    _encoder_prevsample = 0
-    _encoder_previndex = 0
-    for sample in samples:
-        _code, _encoder_prevsample, _encoder_previndex = _encoder(sample, _encoder_prevsample, _encoder_previndex)
-        encoded_samples.append(_code)
+@micropython.viper
+def encode(samples):
+    encoded_samples = array.array("b", samples)
+    _encoder_prevsample:int = 0
+    _encoder_previndex:int = 0
+    for i, sample in enumerate(samples):
+        _state = _encoder(sample, _encoder_prevsample, _encoder_previndex)
+        encoded_samples[i] = int(_state[0])
+        _encoder_prevsample = int(_state[1])
+        _encoder_previndex = int(_state[2])
     return encoded_samples
 
 
-def _encoder(sample, prevsample, previndex):
-    predsample = prevsample
-    index = previndex
-    step = ima_step_table[index]
+@micropython.viper
+def _encoder(sample:int, prevsample:int, previndex:int):
+    predsample:int = prevsample
+    index:int = previndex
+    step_table_ptr = ptr32(ima_step_table)
+    step:int = step_table_ptr[index]
 
     # Compute difference between actual and predicted sample
-    diff = sample - predsample
+    diff:int = sample - predsample
+    code:int = 0
     if diff >= 0:
         code = 0
     else:
@@ -40,7 +53,7 @@ def _encoder(sample, prevsample, previndex):
         diff = -diff
 
     # Quantize difference into 4-bit code using quantizer step size
-    tempstep = step
+    tempstep:int = step
     if diff >= tempstep:
         code |= 4
         diff -= tempstep
@@ -53,7 +66,7 @@ def _encoder(sample, prevsample, previndex):
         code |= 1
 
     # Inverse quantize code into predicted difference using quantizer step size
-    diffq = step >> 3
+    diffq:int = step >> 3
     if code & 4:
         diffq += step
     if code & 2:
@@ -74,7 +87,8 @@ def _encoder(sample, prevsample, previndex):
         predsample = -32768
 
     # Get new quantizer step-size index by adding old index to table lookup
-    index += ima_index_table[code]
+    index_table_ptr = ptr32(ima_index_table)
+    index += index_table_ptr[code]
 
     # Correct quantizer step index overflow
     if index < 0:
@@ -85,25 +99,30 @@ def _encoder(sample, prevsample, previndex):
     return code & 0x0f, predsample, index
 
 
-def decoder(samples):
-    decoded_samples = []
-    _decoder_prevsample = 0
-    _decoder_previndex = 0
-    for sample in samples:
-        _decoder_prevsample, _decoder_previndex = _decoder(sample, _decoder_prevsample, _decoder_previndex)
-        decoded_samples.append(_decoder_prevsample)
+@micropython.viper
+def decode(samples):
+    decoded_samples = array.array("h", samples)
+    _decoder_prevsample:int = 0
+    _decoder_previndex:int = 0
+    for i, sample in enumerate(samples):
+        _state = _decoder(sample, _decoder_prevsample, _decoder_previndex)
+        _decoder_prevsample = int(_state[0])
+        _decoder_previndex = int(_state[1])
+        decoded_samples[i] = _decoder_prevsample
     return decoded_samples
 
 
-def _decoder(code, prevsample, previndex):
-    predsample = prevsample
-    index = previndex
+@micropython.viper
+def _decoder(code:int, prevsample:int, previndex:int):
+    predsample:int = prevsample
+    index:int = previndex
 
     # Find quantizer step size from lookup table
-    step = ima_step_table[index]
+    step_table_ptr = ptr32(ima_step_table)
+    step:int = step_table_ptr[index]
 
     # Inverse quantize code into difference using quantizer step size
-    diffq = step >> 3
+    diffq:int = step >> 3
     if code & 4:
         diffq += step
     if code & 2:
@@ -124,7 +143,8 @@ def _decoder(code, prevsample, previndex):
         predsample = -32768
 
     # Get new quantizer step-size index by adding old index to table lookup
-    index += ima_index_table[code]
+    index_table_ptr = ptr32(ima_index_table)
+    index += index_table_ptr[code]
 
     # Correct quantizer step index overflow
     if index < 0:
